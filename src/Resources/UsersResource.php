43713d0187407b74df4e602dc693772c34fab44d
Administrator <git@wondershare.cn>
2021-05-26 14:16:10 +0800

    public new version

---
+++ src/Resources/UsersResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OnlineUniConvert\Resources;
+
+use OnlineUniConvert\Models\User;
+
+class UsersResource extends AbstractResource
+{
+    /**
+     * @return User
+     * @throws \Http\Client\Exception
+     */
+    public function me(): User
+    {
+        $response = $this->httpTransport->get($this->httpTransport->getBaseUri() . '/me');
+        return $this->hydrator->createObjectByResponse(User::class, $response);
+    }
+}
